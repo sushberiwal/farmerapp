ab623d781f649dae5fc185cedc2086e1a9b94e47
Srishti Bajpai <srishti28bajpai@gmail.com>
2020-01-19 20:40:32 +0530

    code uptil 8:40

--- controllers/buyerController.js
+++ controllers/buyerController.js
@@ -1,9 +1,14 @@
 const buyerModel= require("../models/buyerModel")
 
-module.exports.buyerDetails= function (req,res){
-    res.json({data:"buyer data"})
+module.exports.buyerDetails=function(req,res){
+const {id}=req.params;
+const buyer=await buyerModel.find(id);
+res.json({buyer});
 }
 
-module.exports.updateDetails= function(req,res){
-    res.json({data:"updating v soon"})
+module.exports.updateDetails=function(req,res){
+    const{id}=req.params;
+    const update = req.body;
+    const updatedBuyer=buyerModel.findByIdAndUpdate(id,update,{new:true});
+    res.json({updatedBuyer});
 }
--- controllers/farmerController.js
+++ controllers/farmerController.js
@@ -1,15 +1,16 @@
 
 
 const farmerModel = require("../models/farmerModel")
-module.exports.farmerDetails= async function(req,res)
-{
-    
-    const tempData= await farmerModel.find({})
-    res.json({tempData})
+
+module.exports.farmerDetails= async function(req,res){
+    const {id}=req.params;
+    const farmer=await farmerModel.find(id);
+    res.json({farmer});
 }
 
-
-
-module.exports.updateDetails=  function(req,res){
-    res.json({data:"updating details of the farmer"})
+module.exports.updateDetails=async function(req,res){
+    const{id}=req.params;
+    const update = req.body;
+    const updatedFarmer=bfarmerModel.findByIdAndUpdate(id,update,{new:true});
+    res.json({updatedFarmer});
 }
